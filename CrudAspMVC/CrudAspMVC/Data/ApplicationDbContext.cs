07c756ef7e9c4f8ab058fa7ae543cdbaf092e804
root <a528mail@gmail.com>
2022-01-05 22:32:49 -0600

    primer commit

---
+++ CrudAspMVC/CrudAspMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudAspMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAspMVC.Data;
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+
+        }
+      
+
+    public DbSet<Category> Categories { get; set; }
+    }
